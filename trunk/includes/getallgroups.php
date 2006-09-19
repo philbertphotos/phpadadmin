529b7b273fa529bf9f23dfc688968f9652c9cd0f
james.lloyd <james.lloyd@203a6654-7a1d-0410-bfb7-312270ef6092>
2006-09-19 21:11:57 +0000

    Removed file/folder

--- trunk/includes/getallgroups.php
+++
@@ -1,9 +0,0 @@
-<?php
-$allgroupsou = $allgroupsou.",".$domaindn;
-$allgroupattrs = array("cn","objectclass");
-$allgroupfilter = "(objectclass=group)";
-$allgroupsearch = ldap_search($ad, $allgroupsou, $allgroupfilter, $allgroupattrs)
-          or die ("ldap search failed");
-	 
-$allgroups = ldap_get_entries($ad, $allgroupsearch);
-?>
