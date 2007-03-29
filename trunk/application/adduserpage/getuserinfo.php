84565a8af46bce7955942239357007edea2be6f4
james.lloyd <james.lloyd@203a6654-7a1d-0410-bfb7-312270ef6092>
2007-03-29 17:53:36 +0000

    Removed file/folder

--- trunk/application/adduserpage/getuserinfo.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_GET['user']) && isset($_GET['domain']))
- {
- $user=$phpadadmin->adquery2("(samaccountname=".$_GET['user'].")",array("givenname","initials","sn","displayname","description","office","telephonenumber","mail","wwwhomepage","select","dn","streetaddress","postofficebox","l","st","postalcode","userprincipalname","samaccountname","profilepath","scriptpath","homedirectory","homedrive","homephone","pager","mobile","facsimileTelephoneNumber","ipphone","textarea","title","department","company","manager","directreports","countrycode","info"),$_GET['domain']);
- }
-
-?>
